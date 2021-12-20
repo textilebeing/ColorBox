b9013762748df2084309d1c83933b5745103328d
postfen <26173007+postfen@users.noreply.github.com>
2021-12-20 15:50:52 -0700

    Version 1

---
+++ ColorBox_funcs.py
@@ -0,0 +1,164 @@
+#print("GUI_Funcs_L6F21.py  1.03  211015  S. T. U. Dent") # VER TO BE FIXED
+'''*****************************************************************************
+ This provides a few constants and support functions for the graphics.py-based 
+ GUI labs, to be completed, as well as a bit of test code at the bottom:
+ - COLORS = ( 'black' ... )
+ - [ Rect, Text ] = btn_create(win, x, y, w, h, txt)
+ 
+ The following functions return True ... if:
+ - in_Rectangle(pt, r)      : Point pt is inside Rectangle r
+ - btn_clicked(pt, btn)     : Point pt is inside button btn
+ - in_Circle(pt, c)         : Point pt is inside Circle c
+ - in_Triangle(pt, A, B, C) : pt inside triangle A-B-C.  Complete.
+ 
+EX. USAGE
+- run this to see short test code
+- from GUI_Funcs_L6F21 import *
+ 
+DEPENDENCIES
+ - Graphics50.py
+*****************************************************************************'''
+
+from Graphics50 import *
+
+COLORS = [ 'black', 'red', 'green', 'bluie', 'white' ]
+
+
+'''-----------------------------------------------------------------------------
+ SYNTAX: (Rect, Text) = btn_create(win, x, y, w, h, txt)
+ This returns a tuple consisting of a Rectangle and Text objects, defined by
+ the supplied parameters:
+  - win          : GraphWin-type window in which to crreate the button
+  - x1, y1, w, h : upper-left corner, width, height of Rectangle object
+  - txt          : string displayed centered inside Rectangle
+-----------------------------------------------------------------------------'''
+def btn_create(win, x, y, w, h, txt):
+    rect = Rectangle(Point(x, y), Point(x+w, y+h))
+    rect.draw(win)
+    rect.setFill(color_rgb(146, 131, 116))
+    text = Text( rect.getCenter() , txt)
+    text.setStyle('bold')
+    text.setFace('helvetica')
+    text.setSize(18)
+    text.setFill(color_rgb(235, 219, 178))
+    text.draw(win)
+    return (rect,  text)       # Call this tuple a "button"
+
+'''-----------------------------------------------------------------------------
+ SYNTAX: bool = in_Rectangle(pt, r) : returns True if pt is inside Rectangle r
+ IMP'T this assumes that r.P2() returns (x, y) coordinates which are to the
+ right and down relative to r.P1().
+-----------------------------------------------------------------------------'''
+def in_Rectangle(pt, r):
+    x1, y1 = r.getP1().x , r.getP1().y
+    x2, y2 = r.getP2().x , r.getP2().y
+    return x1 < pt.x < x2 and y1 < pt.y < y2
+
+'''-----------------------------------------------------------------------------
+ SYNTAX: bool = btn_clicked(pt, btn) : returns True if pt.x and pt.y are within
+ the boundaries of the Rectangle component of btn, where: btn: [Rectangle, Text]
+-----------------------------------------------------------------------------'''
+def btn_clicked(pt, btn):
+    r = btn[0]                             # Recall: btn: (Rectangel, Text)
+    p1x, p1y = r.getP1().x, r.getP1().y    # P1: Upper left corner
+    p2x, p2y = r.getP2().x, r.getP2().y    # P2: Lower right corner
+    x, y = pt.x, pt.y
+    return p1x < x < p2x and p1y < y < p2y
+
+
+
+'''-----------------------------------------------------------------------------
+SYNTAX: bool = in_Circle(pt, c) : returns True if Point pt is inside Circle c
+i.e. (pt.x - c.getCircle().x)^2 + (pt.y - c.getCircle().y)^2  <  c.getRadius()^2
+REFs
+- Khan Academy - Circle equation 
+  https://www.khanacademy.org/math/geometry/hs-geo-circles
+         /hs-geo-circle-expanded-equation/a/circle-equation-review
+-----------------------------------------------------------------------------'''
+def  in_Circle(pt, c):
+    center = c.getCenter()
+    distance = ((pt.getX() - center.getX()) ** 2 +\
+                (pt.getY() - center.getY())** 2) ** 0.5
+    
+    return distance < c.radius
+
+'''-----------------------------------------------------------------------------
+ SYNTAX: bool = in_Triangle(pt, A, B, C)     1.03  190908
+ Returns True if Point pt is inside triangle defined by Point objects A, B, C.
+ 
+ Underlying principle: linear algebra.
+ a) given: origin of triangle defined at A, relative to (0, 0), and independent
+    vectors B and C, relative to A, identified by respective points B and C 
+    (yes, purposely named the same): B and C define a line going thru them both
+ b) any other point D on that line can be defined in terms of B and C by:
+      D  = A  +  u * (B-A)  +  v * (C-A) = weighted combin. of vecs B-A and C-A
+    (x,y) components:
+      Dx = Ax + u * (Bx - Ax) + v * (Cx - Ax)                  (1)
+      Dy = Ay + u * (By - Ay) + v * (Cy - Ay)                  (2)
+ c) then: any point between A and B can be defined by: 0 < u < 1 and v = 0
+          any point between A and C can be defined by: 0 < v < 1 and u = 0
+          any point between B and C can be defined by: u + v = 1
+ d) thus any point with u or v outside above ranges is outside triangle A-B-C
+ e) so, given points D, A, B, C, we must determine u and v, starting from (1-2)
+    in matrix form:
+       [ Dx-Ax ] = [ Bx-Ax  Cx-Ax ] x [ u ] := Z x [u]  ==>  [u] = inv(Z) x [Dx]
+       [ Dy-Ax ]   [ By-Ay  Cy-Ay ]   [ v ]        [v]       [v]            [Dy]
+        
+    ==> [u] = [ Cy-Ay  Ax-Cx ] x [Dx-Ax]
+        [v]   [ Ay-By  Bx-Ax ]   [Dy-Ax]
+              ----------------
+                   det(Z)
+    where: det(Z) = (Cy-Ay)*(Bx-Ax) - (Ay-By)*(Ax-Cx)
+    Thus:  u = ( (Cy-Ay)*(Dx-Ax) + (Ax-Cx)*(Dy-Ay) ) / det(Z)
+           v = ( (Ay-By)*(Dx-Ax) + (Bx-Ax)*(Dy-Ay) ) / det(Z)
+           
+    THEREFORE, point D is INSIDE the triangle A-B-C, IF:
+        u > 0 and v > 0 and (u + v < 1)
+        
+ REFs: 
+ - Barycentric Technique, at: http://blackpawn.com/texts/pointinpoly
+ - Barycentric Coordinates and Point in Triangle Tests 
+   https://blogs.msdn.microsoft.com/rezanour/2011/08/07
+          /barycentric-coordinates-and-point-in-triangle-tests/ 
+-----------------------------------------------------------------------------'''
+def in_Triangle(pt, A, B, C):
+    Ax, Bx, Cx, Dx = A.getX(), B.getX(), C.getX(), pt.getX()
+    Ay, By, Cy, Dy = A.getY(), B.getY(), C.getY(), pt.getY()
+    det = (Cy-Ay)*(Bx-Ax) - (Ay-By)*(Ax-Cx)
+    u = ( (Cy-Ay)*(Dx-Ax) + (Ax-Cx)*(Dy-Ay) ) / det
+    v = ( (Ay-By)*(Dx-Ax) + (Bx-Ax)*(Dy-Ay) ) / det
+    #print('u, v = ' + str((u, v)))                         # Dev check
+
+    return u > 0 and v > 0 and (u + v < 1)
+
+#===============================================================================
+# QK TEST CODE
+if __name__ == '__main__':
+    WIN_W , WIN_H = 400, 400
+    win = GraphWin("GUI_Funcs  2021/10/15", WIN_W , WIN_H)
+    
+    A, B, C = Point(200, 100) , Point(300, 200) , Point(100, 300)
+    p = Polygon(A, B, C).draw(win)
+    
+    t = Text(Point(WIN_W//2, WIN_H-40), 
+             "Click anywhere\non triangle\nor near here to Quit!").draw(win)
+    
+    while True:
+        pt = win.getMouse()
+        
+        if t.getAnchor().x -10 < pt.x < t.getAnchor().x + 10  and \
+           t.getAnchor().y -10 < pt.y < t.getAnchor().y + 10:
+            t.setText("CLICK ONCE MORE TO EXIT")
+            break
+        
+        # if in_Triangle(pt, A, B, C):            # A, B, C: triangle vertices
+        # Python trick: Polygon.getPoints() gets list of Point's, and the star 
+        # "unpacks" to 3 separate Point objects for the 3 parameters:
+        if in_Triangle(pt, *p.getPoints()):
+            if p.config['fill'] == "red":  p.setFill('green')
+            else                        :  p.setFill('red')
+    
+    pt = win.getMouse()
+    win.close()
+
+# GUI_Funcs_L6F21.py - LOS ENDOS **********************************************
---
+++ Color_Box_v2.py
@@ -0,0 +1,362 @@
+'''
+                               o
+                          o    |
+                           \   |
+                            \  |
+                             \.|-.
+                             (\|  )
+                    .==================.
+                    | .--------------. |
+                    | |::.::.::.::.::| |
+                    | |'::'::'::'::':| |
+                    | |::.::.::.::.::| |
+                    | |:'::'::'::'::'| |
+                    | |::.::.::.::.::| |
+                    | '--------------'o|
+                    | LI LI """""""   o|
+                    |==================|
+                    |  .------------.  |
+                    | /              \ |
+                    |/                \|
+                    "                  "
+
+
+░█████╗░░█████╗░██╗░░░░░░█████╗░██████╗░██████╗░░█████╗░██╗░░██╗
+██╔══██╗██╔══██╗██║░░░░░██╔══██╗██╔══██╗██╔══██╗██╔══██╗╚██╗██╔╝
+██║░░╚═╝██║░░██║██║░░░░░██║░░██║██████╔╝██████╦╝██║░░██║░╚███╔╝░
+██║░░██╗██║░░██║██║░░░░░██║░░██║██╔══██╗██╔══██╗██║░░██║░██╔██╗░
+╚█████╔╝╚█████╔╝███████╗╚█████╔╝██║░░██║██████╦╝╚█████╔╝██╔╝╚██╗
+░╚════╝░░╚════╝░╚══════╝░╚════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝
+
+'''
+
+
+
+from Graphics50 import *
+from ColorBox_funcs import *
+import random
+from PIL import *
+
+
+'''
+Calls all functions together so the workflow is this:
+- User defines size of window
+- User defines what shapes they want.
+- Program generates a random number of shapes at a random number of sizes
+- Program will generate this shapes with random colors as well
+- Might develop a palette generator? Could be cool
+- Program then writes these shapes to the window and displays them
+- Program will save new image as collage.
+'''
+
+'''
+Tasks/Functions needed:
+1. Create GUI window for user inputs.
+2. Get user inputs
+3. Use user inputs for PIL canvas
+4. Generate shapes and colors.
+5. Put all shapes and colors onto PIL canvas
+6. Display and Save PIL canvas
+
+'''
+
+
+
+class ColorBox():
+    '''
+    Okay, my idea with ColorBox is that I wanted it all under one class,
+    similar to my Annealing Simulator Final. This makes it so variables can
+    be shared between functions. 
+    
+    '''
+    
+    def __init__(self, WIN_H = 500, WIN_W = 500, WIN_TITLE = "ColorBox v2"):
+        '''
+        with the __init__, the object establishes it's size and title.
+        it also establishes some constants that will be used throughout the 
+        class objects functions.
+        
+        '''
+        if (WIN_W or WIN_H) < 500: self.WIN_W = self.WIN_H = 500
+        else:
+            self.WIN_H = WIN_H
+            self.WIN_W = WIN_W
+        
+        self.WIN_TITLE = WIN_TITLE
+        self.BOTTOM = 50
+        self.COLORS = [ 'black', 'red', 'green', 'blue','white', 'beige',
+                       'blue violet', 'dark olive green' ,'dark orange' ,
+                       'deep pink' ,'forest green','lavender', 'khaki',
+                       'rebecca purple', 'plum' ,'peru', 'powder blue']
+        
+        self._GUI_setup() # Set up GUI elements
+        
+     
+    def btn_check(self):
+        '''
+        Checks for click within buttons and performs appropriate actions if true
+        '''
+        
+        if btn_clicked(self.pt, self.btn_Gen):
+            self.click_info.setText("GENERATING...")
+            self.click_info.setTextColor(color_rgb(235, 219, 178))
+            #self.pt = self.win.getMouse()        # Get click, delays quit
+            return True                # Indicates to main that quit clicked
+
+    
+        if btn_clicked(self.pt, self.btn_Quit): 
+            self.click_info.setText("QUITTING...")
+            #self.pt = self.win.getMouse()        # Get click, delays quit
+            return False                # Indicates to main that quit clicked
+
+
+    def display(self, palette = -1):
+        '''
+        Main display function for the color box, contains the main While loop 
+        for display and GUI functions. Maybe temporary? 
+        '''
+
+        self.palette = palette
+        self.click_info.setText("Enter a Number")
+        self.click_info.setTextColor(color_rgb(235, 219, 178))
+        
+        while True: # Main While Loop
+            self.pt = self.win.getMouse()
+            n = self._getEntryNum()
+            if n == None: n = 0
+            
+            if self.btn_check() == False:
+                break
+            
+            if self.btn_check() == True:
+                self.drawer(n)
+                
+    
+    def _getEntryNum(self):
+        '''
+        function for returning number from the entrybox for number of shapes.
+        
+        # TODO
+        # Add in selection for shapes. with feedback eg.  "{shape} off"
+        '''
+        try:
+            n = self.entryBox.getText()
+            n = int(n)
+            return n
+        except:
+            self.click_info.setText("Error, Please Enter a Number")
+
+
+
+
+    def _GUI_setup(self):
+        '''
+        user defines the size of "drawing" that they want generated
+        '''
+        
+        self.win = GraphWin(self.WIN_TITLE, self.WIN_W,\
+                            self.WIN_H + self.BOTTOM)
+        
+        
+        background = Rectangle(Point(0, 0), Point(self.WIN_W, self.WIN_H))
+        background.setFill(color_rgb(60, 56, 54))
+        background.draw(self.win)
+        
+        
+        self.bottom = Rectangle(Point(0, self.WIN_H ), \
+                                Point(self.WIN_W, self.WIN_H+50))
+        self.bottom.setFill(color_rgb(146, 131, 116))
+        self.bottom.draw(self.win)
+        
+
+        self.btn_Gen = btn_create(self.win, 0, self.WIN_H, 
+                                  self.WIN_W*0.33, 50, "GENERATE") # Gen btn
+
+        self.btn_Quit = btn_create(self.win, self.WIN_W*.66, self.WIN_H, 
+                                   self.WIN_W*0.34, 50, "QUIT") #Reset btn
+        
+        
+        self.click_info = Text(Point( self.WIN_W*0.5, self.WIN_H+20), ' ')
+        self.click_info.draw(self.win)  # Display click_info
+        
+        
+        self.entryBox = Entry(Point( self.WIN_W*0.5, self.WIN_H+40), 5)
+        self.entryBox.draw(self.win)
+        
+        return
+
+
+        
+    def circ_gen(self):
+        '''
+        Randomly generates a circle and places it within the window,
+        one circle is generated each time the function is called.
+        '''
+
+        x1 = random.randint(0, self.WIN_W) # Defining random top left start point
+        y1 = random.randint(0, self.WIN_H-30) # top right anywhere from 0 to y2
+        r =  random.randint(0, (self.WIN_H+self.WIN_W)//4)
+        c = Circle(Point(x1, y1), r)
+        
+        self._color_set_and_fill(c)
+    
+    
+    def _color_set_and_fill(self, shape):
+        if self.palette == -1: color = self._color_gen()
+        else: color = self._palette(self.palette)
+        
+        shape.setFill(color_rgb(color[0], color[1], color[2])) 
+        shape.setOutline(color_rgb(color[0], color[1], color[2]))
+        shape.draw(self.win)
+        
+
+
+    def tri_gen(self):
+        '''
+        Function generates amount of triangles, sizes, and placement 
+        in the window.
+        '''
+
+        p1x = random.randint(0, self.WIN_W)
+        p2x = random.randint(0, self.WIN_W)
+        p3x = random.randint(0, self.WIN_W)
+        p1y = random.randint(0, self.WIN_H)
+        p2y = random.randint(0, self.WIN_H)
+        p3y = random.randint(0, self.WIN_H)
+    
+        p = Polygon(Point(p1x, p1y), Point(p2x, p2y), Point(p3x, p3y))
+        self._color_set_and_fill(p)
+
+
+    def oval_gen(self):
+        x1 = random.randint(0, self.WIN_W) # Defining random top left start point
+        y1 = random.randint(0, self.WIN_H) # bottom left anywhere from 0 to y2
+        y2 = random.randint(y1, self.WIN_H) # Defining bottom right end point
+        x2 = random.randint(x1, self.WIN_W) # top right anywhere from top left to end
+        
+        if (abs(x1 - x2)) < 100: x1 -= 20
+        if (abs(y1 - y2)) < 100: y1 -= 20
+        
+        o = Oval(Point(x1, y1), Point(x2, y2))
+        self._color_set_and_fill(o)
+
+    def poly_gen(self):
+        '''
+        Function generates amount of polygons, sizes, and placement 
+        in the window.
+        '''
+        # BLOCK OF COORDINATE GENERATORS
+        p1x = random.randint(0, self.WIN_W)
+        p1y = random.randint(0, self.WIN_H)
+        p2x = random.randint(0, self.WIN_W)
+        p2y = random.randint(0, self.WIN_H)
+        p3x = random.randint(0, self.WIN_W)
+        p3y = random.randint(0, self.WIN_H)
+        p4x = random.randint(0, self.WIN_W)
+        p4y = random.randint(0, self.WIN_H)
+
+        p = Polygon(Point(p1x, p1y), Point(p2x, p2y), Point(p3x, p3y),
+                    Point(p4x, p4y))
+        
+        self._color_set_and_fill(p)
+
+
+    def _random_poly_point_generator(self, n):
+        self.px = []
+        self.py = []
+        for x in range(n):
+            self.px.append(random.randint(0, self.WIN_W))
+            self.py.append(random.randint(0, self.WIN_H))
+        
+        
+        
+        return
+        
+    def rect_gen(self):
+        x1 = random.randint(0, self.WIN_W) # Defining random top left start point
+        y1 = random.randint(0, self.WIN_H) # bottom left anywhere from 0 to y2
+        y2 = random.randint(y1, self.WIN_H) # Defining bottom right end point
+        x2 = random.randint(x1, self.WIN_W) # top right anywhere from top left to end
+        
+        if (abs(x1 - x2)) < 100: x1 -= 20
+        if (abs(y1 - y2)) < 100: y1 -= 20
+        
+        r = Rectangle(Point(x1, y1), Point(x2, y2))
+        self._color_set_and_fill(r)
+
+
+
+
+    def _color_gen(self):
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        color = (r, g, b)
+        return color
+    
+    
+    def _palette(self, palette):
+        '''
+        I guess I'll put palettes in here. It's annoying to add these,
+        so gruvbox will be my test palette. 
+        '''
+        
+        
+        gruvbox = [(40, 40, 40),    # black
+                   (146, 131, 116), # gray1 
+                   (204, 36, 29),   # red1,
+                   (251, 73, 52),   # red2,
+                   (152, 151, 26),  # green1  
+                   (184, 187, 38),  # green2
+                   (215, 153, 33),  # yellow1
+                   (250, 189, 47),  # yellow2
+                   (69, 133, 136),  # blue1
+                   (131, 165, 152), # blue2
+                   (177, 98, 134),  # purple1
+                   (104, 157, 106), # purple2
+                   (142, 192, 124), # aqua1
+                   (168, 153, 132), # aqua2
+                   (235, 219, 178), # gray2
+                   (214, 93, 14),   # orange1
+                   (254, 128, 25),  # orange2
+                   ]
+        
+        
+        if palette == "gruvbox":
+            return random.choice(gruvbox)
+    
+    
+    def drawer(self, n):
+        '''
+        function that draws the shapes to the window, can be called again with 
+        a click in the window, for another drawing.
+        '''
+        background = Rectangle(Point(0, 0), Point(self.WIN_W, self.WIN_H))
+        background.setFill(color_rgb(60, 56, 54))
+        background.draw(self.win)
+        
+        for i in range(n):
+            self.rect_gen()
+            time.sleep(0.01)
+            self.tri_gen()
+            time.sleep(0.01)
+            self.oval_gen()
+            time.sleep(0.01)
+            #self.poly_gen()
+            #time.sleep(0.01)
+        
+        
+        
+        
+        self.click_info.setTextColor(color_rgb(235, 219, 178))
+        self.click_info.setText("Done.")
+
+def test():
+    color_box = ColorBox(100, 100, "ColorBox v2 - NEW (With Buttons!)")
+    color_box.display("gruvbox")
+
+    #color_box.win.getMouse()
+    color_box.win.close()
+
+
+test()
